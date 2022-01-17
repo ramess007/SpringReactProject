d920e1e2e7119d938501c86ef0b8c96017031e62
ramess007 <chapagainramesh1043@gmail.com>
2022-01-16 23:00:45 -0600

    Updated

--- Springbootrestapiproject/src/main/java/com/chapagr/restapi/controller/EmployeeController.java
+++ Springbootrestapiproject/src/main/java/com/chapagr/restapi/controller/EmployeeController.java
@@ -3,9 +3,15 @@ package com.chapagr.restapi.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.AfterDomainEventPublication;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -31,5 +37,21 @@ public class EmployeeController {
 	public Employee saveEmployeeDetails(@RequestBody Employee employee) {
 		return eRepo.save(employee);
 		
+	}
+	
+	@GetMapping("/employees/{id}")
+	public Employee getSingleEmployee(@PathVariable Long id) {
+		return eRepo.findById(id).get();
+	}
+	
+	@PutMapping("/employees")
+	public Employee updateEmployeeDetails(@RequestBody Employee employee) {
+		return eRepo.save(employee);
+	}
+	
+	@DeleteMapping("employees/{id}")
+	public ResponseEntity<HttpStatus> deleteeEmployeeById(@PathVariable Long id) {
+		eRepo.deleteById(id);
+		return new ResponseEntity<HttpStatus>(HttpStatus.NO_CONTENT);
 	}
 }
